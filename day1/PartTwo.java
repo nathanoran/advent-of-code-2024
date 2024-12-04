1ca11f83c6734d20d3aac0845300ccfb28a06e26
Nathan Oran <nathanoran@cox.net>
2024-12-03 21:31:50 -0600

    [AoC-24.1.2] day 1 part 2 solution

---
+++ day1/PartTwo.java
@@ -0,0 +1,44 @@
+package day1;
+
+import java.util.HashMap;
+
+import common.MergeSorter;
+
+public class PartTwo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        ListReader reader = new ListReader();
+        MergeSorter sorter = new MergeSorter();
+
+        Integer[][] lists;
+        lists = reader.readLists("day1/input.txt");
+
+        Integer[] listA = lists[0];
+        HashMap<Integer, Integer> listBOccurences = countValueOccurences(lists[1]);
+
+        Integer totalDistance = 0;
+
+        for (int i = 0; i < listA.length; i++) {
+            if (listBOccurences.get(listA[i]) != null) {
+                totalDistance += listA[i] * listBOccurences.get(listA[i]);
+            }
+        }
+
+        System.out.println("Total Distance: " + totalDistance);
+    }
+
+    protected static HashMap<Integer, Integer> countValueOccurences(Integer[] values) {
+        HashMap<Integer, Integer> valueOccurences = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < values.length; i++) {
+            if (valueOccurences.get(values[i]) == null) {
+                valueOccurences.put(values[i], 1);
+            } else {
+                valueOccurences.put(values[i], valueOccurences.get(values[i]) + 1);
+            }
+        }
+
+        return valueOccurences;
+    }
+}
